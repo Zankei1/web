305bdb17db5fef6bb108b54b3971197ba9bf058f
Gustavo Soares <dev.gustavosoares@gmail.com>
2022-07-25 14:15:21 -0300

    Added lib @tanstack/react-query and initaliazed fetch of users on users page

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
+import { QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
 import { AuthContextProvider } from "./contexts/AuthContext";
 import { CreateUser } from "./pages/CreateUser";
 import { Dashboard } from "./pages/Dashboard";
 import { Login } from "./pages/Login";
 import { Users } from "./pages/Users";
+import { queryClient } from "./services/reactQuery";
 
 export function App() {
   return (
     <BrowserRouter>
       <AuthContextProvider>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/users" element={<Users />} />
-          <Route path="/users/create" element={<CreateUser />} />
-        </Routes>
+        <QueryClientProvider client={queryClient}>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/users" element={<Users />} />
+            <Route path="/users/create" element={<CreateUser />} />
+          </Routes>
+        </QueryClientProvider>
       </AuthContextProvider>
     </BrowserRouter>
   )
--- src/pages/Users/index.tsx
+++ src/pages/Users/index.tsx
@@ -1,18 +1,50 @@
+import { useQuery } from "@tanstack/react-query";
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
 import { AuthContext } from "../../contexts/AuthContext";
+import { api } from "../../services/api";
 import styles from "./styles.module.css";
+
+type User = {
+    id_user: string;
+    name: string;
+    email: string;
+    created_at: string;
+}
 
 export function Users() {
     const { user } = useContext(AuthContext);
-    
+
+    const { data } = useQuery(['users'], async () => {
+        const { data } = await api.get<User[]>("users");
+        
+        console.log(data);
+        
+        const users = data.map((user) => {
+            return {
+                id: user.id_user,
+                name: user.name,
+                email: user.email,
+                createdAt: new Date(user.created_at).toLocaleDateString("pt-BR", {
+                    day: '2-digit',
+                    month: 'long',
+                    year: 'numeric'
+                })
+            }
+        })
+
+        return users;
+    }, {
+        staleTime: 1000 * 60
+    })
+
     return (
         <div className={styles.container}>
             <Sidebar />
             <div className={styles.contentContainer}>
-                <Header userName={user.name} />
+                <Header userName={user?.name} />
                 <div className={styles.tableContainer}>
                     <div className={styles.tableTitle}>
                         <h1>Usuários</h1>
@@ -27,50 +59,15 @@ export function Users() {
                                 <th>Permissões</th>
                             </tr>
                         </thead>
-
                         <tbody className={styles.tableBody}>
-                            <tr>
-                                <td>Gustavo</td>
-                                <td>Gustavo@email.com</td>
-                                <td>Funcionario</td>
-                                <td>Elétrica</td>
-                            </tr>
-                            <tr>
-                                <td>Gustavo</td>
-                                <td>Gustavo@email.com</td>
-                                <td>Funcionario</td>
-                                <td>Elétrica</td>
-                            </tr>
-                            <tr>
-                                <td>Gustavo</td>
-                                <td>Gustavo@email.com</td>
-                                <td>Funcionario</td>
-                                <td>Elétrica, ADM, TI</td>
-                            </tr>
-                            <tr>
-                                <td>Gustavo</td>
-                                <td>Gustavo@email.com</td>
-                                <td>Funcionario</td>
-                                <td>Elétrica</td>
-                            </tr>
-                            <tr>
-                                <td>Gustavo</td>
-                                <td>Gustavo@email.com</td>
-                                <td>Funcionario</td>
-                                <td>Elétrica</td>
-                            </tr>
-                            <tr>
-                                <td>Gustavo</td>
-                                <td>Gustavo@email.com</td>
-                                <td>Funcionario</td>
-                                <td>Elétrica, ADM, TI</td>
-                            </tr>
-                            <tr>
-                                <td>Gustavo</td>
-                                <td>Gustavo@email.com</td>
-                                <td>Funcionario</td>
-                                <td>Elétrica, ADM, TI</td>
-                            </tr>
+                            {data?.map(user => (
+                                <tr key={user.id}>
+                                    <td>{user.name}</td>
+                                    <td>{user.email}</td>
+                                    <td>Funcionario</td>
+                                    <td>Elétrica</td>
+                                </tr>
+                            ))}
                         </tbody>
                     </table>
                 </div>
